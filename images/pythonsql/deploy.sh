06fe90f45f902df7a303c8a095b678976b7afb47
Diana Valladares <96541076+dee-ah-nuh@users.noreply.github.com>
2022-09-06 19:25:07 -0500

    [commented deploy.sh - to rerun container]

--- images/pythonsql/deploy.sh
+++ images/pythonsql/deploy.sh
@@ -3,7 +3,7 @@
 if [ ! -e /opt/.initialized ]
 then
   sudo touch /opt/.initialized
-#  sudo chown -R itversity:itversity /home/itversity/itversity-material
+  sudo chown -R itversity:itversity /home/itversity/itversity-material
 fi
 
 /home/itversity/.local/bin/jupyter lab --ip 0.0.0.0
